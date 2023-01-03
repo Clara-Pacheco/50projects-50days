87e1c4fdca1757c072080710445290dce2362c93
Clara <clarapachecodev@gmail.com>
2023-01-03 12:09:32 -0300

    Capture div with class insert and generate/place the other 3 divs dynamically after a keydown window event

---
+++ day11_event_key_codes/script.js
@@ -0,0 +1,23 @@
+const insert = document.querySelector('.insert')
+console.log(insert)
+
+window.addEventListener('keydown',(event)=>{
+ insert.innerHTML = `
+    <div class="key">
+      ${event.key == ' ' ? 'Space' : event.key}
+      <small>event.key</small>
+    </div>
+     
+    <div class="key">
+      ${event.keyCode}
+      <small>event.keyCode</small>
+    </div>
+
+    <div class="key">
+     ${event.code}
+     <small>event.code</small>
+    </div>
+ `
+})
+
+// Using ``, we can place dynamic values.
