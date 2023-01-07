3e3891578794654d03ef93be21c2dc31fe0f9850
Clara <clarapachecodev@gmail.com>
2023-01-07 10:21:43 -0300

    Add logic to add the amount of small cups drank to the big cup showing the amount drank in percentage and the remained to complete 2L a day

--- day16_drink_water/script.js
+++ day16_drink_water/script.js
@@ -3,6 +3,8 @@ const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
+updateBigCup()
+
 smallCups.forEach((cup,idx)=> {
   cup.addEventListener('click', ()=> highlighCups(idx))
 })
@@ -20,4 +22,28 @@ function highlighCups(idx){ // the index of the cup we have clicked on
       cup.classList.remove('full')
     }
   })
+
+  updateBigCup()
+}
+
+function updateBigCup(){
+  const fullCups = document.querySelectorAll('.cup-small.full').length
+  const totalCups = smallCups.length
+
+ if(fullCups === 0){
+  percentage.style.visibility = 'hidden'
+  percentage.style.height = 0
+ }else {
+  percentage.style.visibility = 'visible'
+  percentage.style.height = `${fullCups / totalCups * 330}px`
+  percentage.innerHTML = `${fullCups / totalCups * 100}%`
+ }
+
+ if(fullCups === totalCups){
+  remained.style.visibility = 'hidden'
+  remained.style.height = 0
+ }else{
+  remained.style.visibility = 'visible'
+  liters.innerHTML = `${2 - (250*fullCups / 1000)}L`
+ }
 }
