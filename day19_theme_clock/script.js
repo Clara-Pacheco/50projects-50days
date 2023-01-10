cdb9b612e60f6f29cd1c8848dea658fc93bd0cd0
Clara <clarapachecodev@gmail.com>
2023-01-10 08:09:55 -0300

    Implement logic for clock needles and add transition to them

---
+++ day19_theme_clock/script.js
@@ -0,0 +1,45 @@
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggle= document.querySelector('.toggle')
+
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+toggle.addEventListener('click',(e)=>{
+  document.querySelector('html').classList.toggle('dark')
+  if(document.querySelector('html').classList.contains('dark')){
+    e.target.innerHTML = 'Light Mode'
+  }else {
+    e.target.innerHTML = 'Dark Mode'
+  }
+  
+})
+
+function setTime(){
+  const time = new Date()
+  const month = time.getMonth()
+  const day = time.getDay()
+  const hours = time.getHours()
+  const hoursForClock = hours % 12   // because it´s a 12h clock, not 24h
+  const minutes = time.getMinutes()
+  const seconds = time.getSeconds()
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+}
+
+
+// StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime()
+
+setInterval(setTime,1000)
