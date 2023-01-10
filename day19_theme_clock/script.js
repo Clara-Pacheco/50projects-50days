f975b00a37a79ef5e40dd08372f1d12d1a7275e9
Clara <clarapachecodev@gmail.com>
2023-01-10 08:33:08 -0300

    Implement logic to change de digital hour/day/month + adjust day circle class and delete info hard coded in HTML

--- day19_theme_clock/script.js
+++ day19_theme_clock/script.js
@@ -2,7 +2,7 @@ const hourEl = document.querySelector('.hour')
 const minuteEl = document.querySelector('.minute')
 const secondEl = document.querySelector('.second')
 const timeEl = document.querySelector('.time')
-const dateEl = document.querySelector('.date')
+let dateEl = document.querySelector('.date')
 const toggle= document.querySelector('.toggle')
 
 
@@ -23,14 +23,20 @@ function setTime(){
   const time = new Date()
   const month = time.getMonth()
   const day = time.getDay()
+  const date = time.getDate()
   const hours = time.getHours()
   const hoursForClock = hours % 12   // because it´s a 12h clock, not 24h
   const minutes = time.getMinutes()
   const seconds = time.getSeconds()
+  const ampm = hours >= 12 ? 'PM':'AM'
 
   hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
   minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
   secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+  timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}`: minutes} ${ampm}`
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+
 
 }
 
